7a4cdd377899e4fa1deee86146f59e7666bd523e
Jiachen Sun <sunjiachen1110@gmail.com>
2024-08-20 21:42:43 -0700

    more photos and doodles

--- app/utils/doodle-urls.ts
+++ app/utils/doodle-urls.ts
@@ -23,5 +23,20 @@ export const images = [
     src: "/media/doodles/tree.jpg",
     width: 803,
     height: 821
+  },
+  {
+    src: "/media/doodles/oski.jpg",
+    width: 1047,
+    height: 1480
+  },
+  {
+    src: "/media/doodles/rosebud.jpg",
+    width: 1492,
+    height: 1048
+  },
+  {
+    src: "/media/doodles/shell.jpg",
+    width: 1261,
+    height: 892
   }
 ]
--- app/utils/photo-urls.ts
+++ app/utils/photo-urls.ts
@@ -15,24 +15,14 @@ export const images = [
     height: 987
   },
   {
-    src: "/media/photos/kwason.jpg",
-    width: 2059,
-    height: 1159
-  },
-  {
     src: "/media/photos/red-canyon.jpg",
     width: 998,
     height: 1331
   },
   {
-    src: "/media/photos/ellsworth-sunset.jpg",
-    width: 719,
-    height: 1100
-  },
-  {
-    src: "/media/photos/sakura.jpg",
-    width: 998,
-    height: 1331
+    src: "/media/photos/kwason.jpg",
+    width: 335,
+    height: 557
   },
   {
     src: "/media/photos/israel-protest.jpg",
@@ -43,5 +33,70 @@ export const images = [
     src: "/media/photos/tiggy-bb.jpg",
     width: 2059,
     height: 1159
+  },
+  {
+    src: "/media/photos/wood.jpg",
+    width: 1280,
+    height: 720
+  },
+  {
+    src: "/media/photos/palm-trees.jpg",
+    width: 720,
+    height: 1280
+  },
+  {
+    src: "/media/photos/diner-lights.jpg",
+    width: 1280,
+    height: 720
+  },
+  {
+    src: "/media/photos/boardwalk.jpg",
+    width: 720,
+    height: 1280
+  },
+  {
+    src: "/media/photos/cablecar.jpg",
+    width: 1280,
+    height: 720
+  },
+  {
+    src: "/media/photos/moss.jpg",
+    width: 1280,
+    height: 720
+  },
+  {
+    src: "/media/photos/grassy-lumps.jpg",
+    width: 1280,
+    height: 720
+  },
+  {
+    src: "/media/photos/bigben.jpg",
+    width: 720,
+    height: 1280
+  },
+  {
+    src: "/media/photos/venice-banksy.jpg",
+    width: 720,
+    height: 1280
+  },
+  {
+    src: "/media/photos/venice-night-1.jpg",
+    width: 1280,
+    height: 720
+  },
+  {
+    src: "/media/photos/venice-gelato.jpg",
+    width: 720,
+    height: 1280
+  },
+  {
+    src: "/media/photos/venice-night-2.jpg",
+    width: 1280,
+    height: 720
+  },
+  {
+    src: "/media/photos/venice-seagull.jpg",
+    width: 590,
+    height: 1280
   }
 ]
